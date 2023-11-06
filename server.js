2d5efb6408d8f4f0cb10d855cd56d29b08127c1d
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-11-06 17:54:31 -0500

    server.js setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const mongoose = require('mongoose'); //mongoose initialized
+const routes = require('./routes');
+const db = require('./config/connection');
+const PORT = process.env.PORT || 2641;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+db.once('open', () => { //.once() is a mongoose method which indicates the connetion to mongodb is made
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+});
