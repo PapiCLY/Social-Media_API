61fa86f6fe610a32e5d31645188f19cf27cc3a69
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-11-06 18:47:46 -0500

    updated thought route file

---
+++ routes/api/thoughtRoute.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+// /api/thoughts/:id
+router
+    .route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction)
+
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReaction);
+
+module.exports = router;
